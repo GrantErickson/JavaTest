87e008430adf37903727261afa398dd457ad3aac
Grant Erickson <grant@granterickson.com>
2021-06-09 14:29:19 -0700

    Failing test

--- src/test/java/javatest/TestDoStuff.java
+++ src/test/java/javatest/TestDoStuff.java
@@ -10,7 +10,12 @@ public class TestDoStuff {
     public void testDoStuff() {
         DoStuff doStuff = new DoStuff(1, 2);
         assertEquals(2, doStuff.Multiply());
-        assertEquals(3, doStuff.Multiply());
+    }
+
+    @Test
+    public void testDoStuffFail() {
+        DoStuff doStuff = new DoStuff(5, -1);
+        assertEquals(5, doStuff.Multiply());
     }
 
 }
